fffb08e39fd60f3270a7eac74f476c2fcb322167
jonathan <jonathanvm13@gmail.com>
2015-04-19 19:30:31 -0500

    Update scorm added

--- controllers/scorm.js
+++ controllers/scorm.js
@@ -29,6 +29,23 @@ module.exports = {
             //helper.createManifest();
         }
     ],
+    update: [
+        function (req, res) {
+            var json = JSON.parse(parser.toJson(req.body.question));
+            var meta = JSON.parse(parser.toJson(req.body.metadatos));
+
+            fs.writeFile("./scorm-template/js/xml-question.js", "var question = "+JSON.stringify(json.xml)+"; window.question = window.question || question;", function(err) {
+                if(err) return res.status(500).jsonp({ok: false});
+
+                fs.writeFile("./scorm-template/js/xml-metadatos.js", "module.exports = "+JSON.stringify(meta.xml), function(err) {
+                    if(err) return res.status(500).jsonp({ok: false});
+
+                    return res.status(200).jsonp({ok: true});
+                });
+            });
+            //helper.createManifest();
+        }
+    ],
     Download:[
         function (req, res) {
             var file = './scorm.zip';
