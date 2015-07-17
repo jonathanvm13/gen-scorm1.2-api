d3695396f877865a2fc785d1471368423995f768
Jonathan Vallejo Muñoz <jonathanvm13@gmail.com>
2015-07-17 16:10:47 -0500

    ordering routes

--- routes/routes.js
+++ routes/routes.js
@@ -6,25 +6,25 @@ var Scorm = require('../controllers/scorm'),
 var routes = [
 
   //Scorm
-  {path: '/scorms/',                 httpMethod: 'POST',   middleware: [Scorm.zipAndDownloadFile]},
-  {path: '/scorms/',                 httpMethod: 'PUT',    middleware: [Scorm.update]},
-  {path: '/scorms/download',         httpMethod: 'GET',    middleware: [Scorm.Download]},
-  {path: '/scorms/uploadfiles',      httpMethod: 'POST',   middleware: [Scorm.uploadFiles]},
+  {path: '/scorms/',                     httpMethod: 'POST',   middleware: [Scorm.zipAndDownloadFile]},
+  {path: '/scorms/',                     httpMethod: 'PUT',    middleware: [Scorm.update]},
+  {path: '/scorms/download',             httpMethod: 'GET',    middleware: [Scorm.Download]},
+  {path: '/scorms/uploadfiles',          httpMethod: 'POST',   middleware: [Scorm.uploadFiles]},
 
   // Folder
-  {path: '/folders',                 httpMethod: 'POST',   middleware: [FolderCtrl.create]},
-  {path: '/folders/:folderid',             httpMethod: 'PUT',    middleware: [FolderCtrl.update]},
-  {path: '/folders/:folderid',             httpMethod: 'DELETE', middleware: [FolderCtrl.delete]},
-  {path: '/folders',                 httpMethod: 'GET',    middleware: [FolderCtrl.list]},
+  {path: '/folders',                     httpMethod: 'POST',   middleware: [FolderCtrl.create]},
+  {path: '/folders/:folderid',           httpMethod: 'PUT',    middleware: [FolderCtrl.update]},
+  {path: '/folders/:folderid',           httpMethod: 'DELETE', middleware: [FolderCtrl.delete]},
+  {path: '/folders',                     httpMethod: 'GET',    middleware: [FolderCtrl.list]},
 
   //Question
-  {path: '/folders/:folderid/questions',httpMethod: 'POST',   middleware: [QuestionCtrl.createQuestion]},
-  {path: '/questions/:questionid',           httpMethod: 'PUT',    middleware: [QuestionCtrl.updateQuestion]},
-  {path: '/questions/:questionid',           httpMethod: 'DELETE', middleware: [QuestionCtrl.deleteQuestion]},
+  {path: '/folders/:folderid/questions', httpMethod: 'POST',   middleware: [QuestionCtrl.createQuestion]},
+  {path: '/questions/:questionid',       httpMethod: 'PUT',    middleware: [QuestionCtrl.updateQuestion]},
+  {path: '/questions/:questionid',       httpMethod: 'DELETE', middleware: [QuestionCtrl.deleteQuestion]},
 
   //User
-  {path: '/users',                   httpMethod: 'POST',   middleware: [UserCtrl.create]},
-  {path: '/users/login',             httpMethod: 'POST',   middleware: [UserCtrl.login]},
+  {path: '/users',                       httpMethod: 'POST',   middleware: [UserCtrl.create]},
+  {path: '/users/login',                 httpMethod: 'POST',   middleware: [UserCtrl.login]},
 
 ];
 
